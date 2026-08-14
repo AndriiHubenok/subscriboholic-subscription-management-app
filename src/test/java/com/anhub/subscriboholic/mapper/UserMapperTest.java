388d75b326bca6aa9a08673a8f150910891aa756
AndriiHubenok <andrewhu08@gmail.com>
2026-08-14 22:36:23 +0300

    fix UserMapper 2

--- src/test/java/com/anhub/subscriboholic/mapper/UserMapperTest.java
+++ src/test/java/com/anhub/subscriboholic/mapper/UserMapperTest.java
@@ -6,18 +6,16 @@ import com.anhub.subscriboholic.model.entity.User;
 import com.anhub.subscriboholic.model.enumerated.UserRole;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class UserMapperTest {
 
+    @Autowired
     private UserMapper userMapper;
-
-    @BeforeEach
-    void setUp() {
-
-        userMapper = new UserMapperImpl();
-    }
 
     @Test
     void shouldMapEntityToDto() {
