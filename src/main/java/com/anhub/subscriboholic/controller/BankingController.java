d88455f4c9f646d3a14264c8eb441f42ccb0c1ec
AndriiHubenok <andrewhu08@gmail.com>
2026-09-04 23:42:17 +0300

    auth enable banking api

--- src/main/java/com/anhub/subscriboholic/controller/BankingController.java
+++ src/main/java/com/anhub/subscriboholic/controller/BankingController.java
@@ -5,10 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @AllArgsConstructor
@@ -20,6 +22,8 @@ public class BankingController {
     public ResponseEntity<String> fetchBankData() {
         String authHeader = bankingAuthService.getAuthorizationHeader();
 
+        System.out.println("authHeader: " + authHeader);
+
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(authHeader.substring("Bearer ".length()));
         headers.setAccept(List.of(MediaType.APPLICATION_JSON));
@@ -29,8 +33,46 @@ public class BankingController {
         RestTemplate restTemplate = new RestTemplate();
 
         return restTemplate.exchange(
-                "https://api.enablebanking.com/aspsps?country=DE",
+                "https://api.enablebanking.com/aspsps",
                 HttpMethod.GET,
+                entity,
+                String.class
+        );
+    }
+
+    @PostMapping("/api/bank-auth")
+    public ResponseEntity<String> authBanking() {
+        String authHeader = bankingAuthService.getAuthorizationHeader();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(authHeader.substring("Bearer ".length()));
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        String validUntil = java.time.Instant.now()
+                .plus(90, java.time.temporal.ChronoUnit.DAYS)
+                .toString();
+
+        Map<String, Object> requestBody = new java.util.HashMap<>();
+
+        requestBody.put("access", java.util.Map.of("valid_until", validUntil));
+        requestBody.put("aspsp", java.util.Map.of("name", "Nordea", "country", "FI"));
+        requestBody.put("state", java.util.UUID.randomUUID().toString());
+        requestBody.put("redirect_url", "http://localhost:8080/enable_banking_callback");
+
+        // Optional params
+//        requestBody.put("psu_type", "personal");
+//        requestBody.put("auth_method", "methodName");
+//        requestBody.put("credentials", java.util.Map.of("userId", "MyUsername"));
+//        requestBody.put("credentials_autosubmit", true);
+//        requestBody.put("language", "en");
+//        requestBody.put("psu_id", "string");
+
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(requestBody, headers);
+        RestTemplate restTemplate = new RestTemplate();
+
+        return restTemplate.postForEntity(
+                "https://api.enablebanking.com/auth",
                 entity,
                 String.class
         );
